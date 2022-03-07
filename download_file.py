1363c91d808f6852ca0b7e283aeca314f0d30f0d
Weng Kee Teh <wengkee.teh@gmail.com>
2022-03-07 17:26:08 +0800

    remove comments from download py

--- download_file.py
+++ download_file.py
@@ -1,17 +1,6 @@
 import os
 from minio import Minio
 
-'''
-    1. Install MinIO Python SDK
-    pip3.9 install minio
-
-    2. Export Environment Variables
-    source export_env.sh
-
-    3. MinIO Python SDK API
-    https://docs.min.io/docs/python-client-api-reference.html
-'''
-
 def get_s3_server():
     minioClient = Minio(os.environ['S3_ENDPOINT_URL'],
                         access_key=os.environ['AWS_ACCESS_KEY_ID'],
